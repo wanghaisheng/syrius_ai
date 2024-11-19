6125c684c8a901abee63f98c9e910df71e6267fd
Romain Portanguen <portanguenr@gmail.com>
2024-11-19 12:05:49 +0100

    Refactor LLM service structure by moving interfaces to a dedicated folder and implementing prompt generator services for OpenAI, Anthropic, and Mistral models

--- apps/api/src/app/factories/llm-service-factory.ts
+++ apps/api/src/app/factories/llm-service-factory.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ChatOpenAI } from '@langchain/openai';
 import { ChatAnthropic } from '@langchain/anthropic';
 import { ChatMistralAI } from '@langchain/mistralai';
@@ -7,20 +7,39 @@ import {
   mistralConfiguration,
   openAIConfiguration,
 } from '../config/llm.config';
+import { GeneralLLMService } from '../services/llm/general-llm.service';
+import { ILLMService } from '../services/llm/interfaces/llm.service.requirements';
 import { ILLMServiceFactory } from './llm-service-factory.requirements';
-import { ILLMService } from '../services/llm/llm.service.requirements';
-import { GeneralLLMService } from '../services/llm/llm.service';
+import { ISystemPromptGenerator } from '../services/llm/interfaces/system-prompt-generator.service.requirements';
 
 @Injectable()
 export class LLMServiceFactory implements ILLMServiceFactory {
+  constructor(
+    @Inject('IOpenAIPromptGenerator')
+    private readonly openAIPromptGenerator: ISystemPromptGenerator,
+    @Inject('IAnthropicPromptGenerator')
+    private readonly anthropicPromptGenerator: ISystemPromptGenerator,
+    @Inject('IMistralPromptGenerator')
+    private readonly mistralPromptGenerator: ISystemPromptGenerator
+  ) {}
+
   create(model: 'openai' | 'anthropic' | 'mistral'): ILLMService {
     switch (model) {
       case 'openai':
-        return new GeneralLLMService(new ChatOpenAI(openAIConfiguration));
+        return new GeneralLLMService(
+          new ChatOpenAI(openAIConfiguration),
+          this.openAIPromptGenerator
+        );
       case 'anthropic':
-        return new GeneralLLMService(new ChatAnthropic(anthropicConfiguration));
+        return new GeneralLLMService(
+          new ChatAnthropic(anthropicConfiguration),
+          this.anthropicPromptGenerator
+        );
       case 'mistral':
-        return new GeneralLLMService(new ChatMistralAI(mistralConfiguration));
+        return new GeneralLLMService(
+          new ChatMistralAI(mistralConfiguration),
+          this.mistralPromptGenerator
+        );
       default:
         throw new Error(`Model not supported: ${model}`);
     }
